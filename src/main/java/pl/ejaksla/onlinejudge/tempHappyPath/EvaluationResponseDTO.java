de73fa4fe1eba1bd620905e0b241d73f952722a3
Jakub Slawinski <jaksla92@gmail.com>
2019-05-16 00:33:19 +0200

    focusing on backend, happy path impl #5

---
+++ src/main/java/pl/ejaksla/onlinejudge/tempHappyPath/EvaluationResponseDTO.java
@@ -0,0 +1,50 @@
+package pl.ejaksla.onlinejudge.tempHappyPath;
+
+import java.util.StringJoiner;
+import static com.google.common.base.Preconditions.checkArgument;
+
+public class EvaluationResponseDTO {
+    /**
+     * Response status
+     */
+    public enum Status {
+        OK,
+        ERROR
+    }
+
+    private Status status;
+    private String message;
+
+    /**
+     * @param status  the status, required
+     * @param message the message, required
+     */
+    EvaluationResponseDTO(final Status status, final String message) {
+        checkArgument(status != null, "status is required");
+        checkArgument(message != null, "message is required");
+        this.status = status;
+        this.message = message;
+    }
+
+    /**
+     * @return the status, not null
+     */
+    public Status getStatus() {
+        return status;
+    }
+
+    /**
+     * @return the message, not null
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", EvaluationResponseDTO.class.getSimpleName() + "[", "]")
+                .add("status=" + status)
+                .add("message='" + message + "'")
+                .toString();
+    }
+}
